0a28f9d241989481330aaf851115b70a352a0cba
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-01-08 22:34:24 +0900

    react-prac
    react-prac

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <header>
+            <h1>
+                <a
+                    href="/"
+                    onClick={function (event) {
+                        event.preventDefault();
+                        props.onChnageMode();
+                    }}
+                >
+                    {props.title}
+                </a>
+            </h1>
+        </header>
+    );
+}
+function Nav(props) {
+    const lis = [];
+    for (let i = 0; i < props.topics.length; i++) {
+        let t = props.topics[i];
+        lis.push(
+            <li key={t.id}>
+                <a
+                    id={t.id}
+                    href={'/read' + t.id}
+                    onClick={(event) => {
+                        event.preventDefault();
+                        props.onChnageMode(event.target.id);
+                    }}
+                >
+                    {t.title}
+                </a>
+            </li>
+        );
+    }
+    return <ol>{lis}</ol>;
+}
+function Article(props) {
+    return (
+        <article>
+            <h2>{props.title}</h2>
+            {props.body}
+        </article>
+    );
+}
+function App() {
+    const topics = [
+        { id: 1, title: 'html', body: 'html is...' },
+        { id: 2, title: 'css', body: 'css is...' },
+        { id: 3, title: 'javascript', body: 'js is...' },
+    ];
+    return (
+        <div>
+            <Header
+                title="react"
+                onChnageMode={function () {
+                    alert('헤더입니다');
+                }}
+            />
+            <Nav
+                topics={topics}
+                onChnageMode={(id) => {
+                    alert(id);
+                }}
+            />
+            <Article title="Welcome" body="hello, WEB" />
+        </div>
+    );
+}
+
+export default App;
