7edea260ff8999c237fb2139ca26e6e7440ed16b
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-01-15 00:50:11 +0900

    studying state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 // 함수 안 {} 는 표현식으로 해석됨.
 function Header(props) {
@@ -33,7 +34,7 @@ function Nav({ topics, onChangeMode }) {
           href={'/read/' + t.id}
           onClick={(event) => {
             event.preventDefault();
-            onChangeMode(event.target.id);
+            onChangeMode(Number(event.target.id));
           }}
         >
           {t.title}
@@ -58,26 +59,49 @@ function Article(props) {
 }
 
 function App() {
+  //   const _mode = useState('WELCOME');
+  //   const mode = _mode[0];
+  //   const setMode = _mode[1];
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+
   const topics = [
     { id: 1, title: 'html', body: 'html is...' },
     { id: 2, title: 'css', body: 'css is...' },
     { id: 3, title: 'js', body: 'js is...' },
   ];
+
+  let content = null;
+  if (mode === 'WELCOME') {
+    content = <Article title="WELCOME" body="Hello, WEB" />;
+  } else if (mode === 'READ') {
+    let title,
+      body = null;
+    for (let i = 0; i < topics.length; i++) {
+      console.log(topics[i].id, id);
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body} />;
+  }
   return (
     <div>
       <Header
         title="WEB"
         onChangeMode={() => {
-          alert('Header');
+          setMode('WELCOME');
         }}
       />
       <Nav
         topics={topics}
-        onChangeMode={(id) => {
-          alert(id);
+        onChangeMode={(_id) => {
+          setMode('READ');
+          setId(_id);
         }}
       />
-      <Article title="WELCOME" body="Hello, WEB" />
+      {content}
     </div>
   );
 }
