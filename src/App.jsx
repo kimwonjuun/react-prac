2ff3b2413c6259b2fe36109ce86218ed79d826a3
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-01-14 17:36:53 +0900

    prettier setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
+function Header() {
+  return (
+    <header>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+}
+
 function App() {
-    return <></>;
+  return (
+    <div>
+      <Header />
+      <nav>
+        <ol>
+          <a href="/read/1">
+            <li>html</li>
+          </a>
+          <a href="/read/2">
+            <li>css</li>
+          </a>
+          <a href="/read/3">
+            <li>js</li>
+          </a>
+        </ol>
+      </nav>
+      <article>
+        <h2>Welcom</h2>
+        Hello, WEB
+      </article>
+    </div>
+  );
 }
 
 export default App;
