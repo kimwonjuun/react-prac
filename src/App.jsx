5f49c21e6735c8542d93434b11912d20a5a8c77b
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-01-14 17:43:43 +0900

    studying

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,46 @@
 import React from 'react';
 
-function Header() {
+function Header(props) {
   return (
     <header>
       <h1>
-        <a href="/">WEB</a>
+        <a href="/">{props.title}</a>
       </h1>
     </header>
+  );
+}
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <a href="/read/1">
+          <li>html</li>
+        </a>
+        <a href="/read/2">
+          <li>css</li>
+        </a>
+        <a href="/read/3">
+          <li>js</li>
+        </a>
+      </ol>
+    </nav>
+  );
+}
+function Article() {
+  return (
+    <article>
+      <h2>Welcom</h2>
+      Hello, WEB
+    </article>
   );
 }
 
 function App() {
   return (
     <div>
-      <Header />
-      <nav>
-        <ol>
-          <a href="/read/1">
-            <li>html</li>
-          </a>
-          <a href="/read/2">
-            <li>css</li>
-          </a>
-          <a href="/read/3">
-            <li>js</li>
-          </a>
-        </ol>
-      </nav>
-      <article>
-        <h2>Welcom</h2>
-        Hello, WEB
-      </article>
+      <Header title="react" />
+      <Nav />
+      <Article />
     </div>
   );
 }
